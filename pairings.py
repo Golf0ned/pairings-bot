9c6b0774902635f68871e2cfead7adab81fbe7f2
Golf0ned <benjaminye.email@gmail.com>
2023-11-16 12:58:50 -0600

    add pile of utilities

--- pairings.py
+++ pairings.py
@@ -8,34 +8,37 @@ validRounds = ["1", "2", "3", "4", "5", "6", "7", "8",
 class PairingsManager():
 
     def __init__(self):
-        self.school = "Northwestern"
-        self.tournamentID = 0
-        self.roundManager = RoundManager()
-        self.blasting = False
+        self.__school = "Northwestern"
+        self.__tournamentID = 0
+        self.__roundManager = RoundManager()
+        self.__blasting = False
+        self.__blastChannel = ""
 
-    def setSchool(self, school : str):
-        self.school = school
+    def setSchool(self, school : str): self.__school = school
 
-    def setTournament(self, id : int):
+    def getSchool(self): return self.__school
+
+    def setTournament(self, id : int): 
         if id < 0:
             # TODO: error checking for id >= 0
             pass
-        self.tournamentID = id
+        self.__tournamentID = id
+    
+    def isBlasting(self): return self.__blasting
+        
+    def startBlasting(self): self.__blasting = True
+
+    def stopBlasting(self): self.__blasting = False
+
+    def setBlastChannel(self, id : str): self.__blastChannel = id
+
+    def getBlastChannel(self): return self.__blastChannel
 
     def getRoundURL(self, round):
         if round not in validRounds:
             # TODO: error checking for invalid round 
             pass
-        return f'https://www.tabroom.com/index/tourn/postings/index.mhtml?tourn_id={self.tournamentID}&round_id={round}'
-    
-    def isBlasting(self):
-        return self.blasting
-        
-    def startBlasting(self):
-        self.blasting = True
-
-    def stopBlasting(self):
-        self.blasting = False
+        return f'https://www.tabroom.com/index/tourn/postings/index.mhtml?tourn_id={self.__tournamentID}&round_id={round}'
 
 
 class RoundManager():
