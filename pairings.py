bb2f5755b438c083c6b0fc36bd3140d306b7f0be
Golf0ned <benjaminye.email@gmail.com>
2023-11-17 15:43:13 -0600

    minor code cleanup

--- pairings.py
+++ pairings.py
@@ -1,31 +1,35 @@
 # pairings.py
-import asyncio
-
 import tournament
 
 class PairingsManager():
 
     def __init__(self):
-
+        self.__tournament = None
         self.__school = ''
         self.__blastChannel = ''
         self.__blasting = False
         self.__hasBlast = False
 
-        self.__tournament = None
-        self.__hasTournament = False
-
-
+    def initTournament(self, id : str):
+        self.__tournament = tournament.TournamentManager(self.__school, id)
+        self.__tournament.checkFirstRound()
+    
+    def hasTournament(self): 
+        return True if self.__tournament else False
+    def getTournamentID(self): return self.__tournament.getTournamentID()
+    def getCurRound(self): return self.__tournament.getRound()
 
     def setSchool(self, school : str): self.__school = school
     def getSchool(self): return self.__school
 
-
     def isBlasting(self): return self.__blasting
+
     def startBlasting(self): self.__blasting = True
     def stopBlasting(self): self.__blasting = False
+
     def setBlastChannel(self, id : str): self.__blastChannel = id
     def getBlastChannel(self): return self.__blastChannel
+
     def hasBlast(self): 
         if self.__hasBlast:
             self.__hasBlast = False
@@ -33,19 +37,6 @@ class PairingsManager():
         return False
 
 
-    def initTournament(self, id : str):
-        self.__tournament = tournament.TournamentManager(self.__school, id)
-        self.__hasTournament = True
-        self.__tournament.checkFirstRound()
-
-    def hasTournament(self):
-        return self.__hasTournament
-
-    def getTournamentID(self): return self.__tournament.getTournamentID()
-    # def getTournamentName(self): return self.__tournament.getTournamentName()
-    # def getTournamentTeams(self): return self.__tournament.getTournamentTeams()
-
-    def getCurRound(self): return self.__tournament.getRound()
 
     def checkForRound():
         pass
